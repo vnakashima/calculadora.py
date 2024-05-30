a8ffe7c6eee56321a887993e2d484dc72136e617
vnakashima <vnakashima08@gmail.com>
2024-05-29 23:25:31 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+num1 = float (input("Digite o primeiro numero:"))
+
+num2= float (input("Digite o segundo numero:"))
+
+print ("resultados da adição:", num1 + num2)
+
+print("resultados da subtração:", num1 - num2)
+
+print("resultados da  multplicaçâo:", num1 * num2)
+
+print("resultados da divisâo:", num1 % num2)
+
